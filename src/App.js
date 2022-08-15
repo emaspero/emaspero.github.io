8e475695ec383b1af2acce1dca9f27dd2c6ac983
Elisabetta <emaspero@hotmail.com>
2022-08-15 15:57:04 +0100

    Worked on header and about section

--- src/App.js
+++ src/App.js
@@ -2,172 +2,45 @@ import React from "react";
 import "./App.css";
 import Timeline from "./timeline/Timeline";
 import ContactForm from "./contact/ContactForm";
+import About from "./about/About";
 
 export default function App() {
+
   return (
     <div className="home-container">
       <header id="home">
+      {/* <img
+          src="https://dummyimage.com/640x360"
+          alt="placeholder"
+          className="about-pic"
+        /> */}
         <h1>
           <span>Hi, I'm </span>Elisabetta Maspero.
         </h1>
         <p>Polyglot Junior Software Engineer.</p>
-        <img
+        <div className="box-container">
+        <div className="box fill">
+          <a href="https://www.linkedin.com/in/elisabetta-maspero/" target="_blank" rel="noopener noreferrer">
+            <img src="/imgs/linkedin.png" alt="linkedin" id="box-logo" />
+          </a>
+        </div>
+        <div className="box fill">
+        <a href="https://github.com/emaspero" target="_blank" rel="noopener noreferrer">
+          <img src="/imgs/github.png" alt="github" id="box-logo" />
+        </a>
+        </div>
+        </div>
+        {/* <img
           src="https://dummyimage.com/640x360"
           alt="placeholder"
           className="about-pic"
-        />
+        /> */}
       </header>
 
       <div id="about">
-        <h2>About Me</h2>
-        <hr />
-        <p>
-          I have 3 years experience in managing sales and international clients
-          in the UK’s New Homes industry and a bachelor degree in Languages. My
-          interest in coding has been lingering since my early teenage years
-          where I used to run a blog and play around with basic HTML/CSS skills.
-          I believe that languages and programming languages have a lot in
-          common. The problem-solving, analytical world of programming, with its
-          endless learning possibilities, perfectly chimes with my attitude and
-          my strong desire to develop and widen my knowledge.
-        </p>
-
-        <h3>My Skills</h3>
-        <hr />
-        <div>
-          <a
-            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg"
-              width="36"
-              height="36"
-              alt="JavaScript"
-            />
-          </a>
-          <a href="https://www.python.org/" target="_blank" rel="noreferrer">
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/python-colored.svg"
-              width="36"
-              height="36"
-              alt="Python"
-            />
-          </a>
-          <a
-            href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg"
-              width="36"
-              height="36"
-              alt="HTML5"
-            />
-          </a>
-          <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg"
-              width="36"
-              height="36"
-              alt="React"
-            />
-          </a>
-          <a href="https://jquery.com/" target="_blank" rel="noreferrer">
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/jquery-colored.svg"
-              width="36"
-              height="36"
-              alt="JQuery"
-            />
-          </a>
-          <a
-            href="https://www.w3.org/TR/CSS/#css"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg"
-              width="36"
-              height="36"
-              alt="CSS3"
-            />
-          </a>
-          <a href="https://getbootstrap.com/" target="_blank" rel="noreferrer">
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg"
-              width="36"
-              height="36"
-              alt="Bootstrap"
-            />
-          </a>
-          <a href="https://nodejs.org/en/" target="_blank" rel="noreferrer">
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg"
-              width="36"
-              height="36"
-              alt="NodeJS"
-            />
-          </a>
-          <a href="https://expressjs.com/" target="_blank" rel="noreferrer">
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-colored.svg"
-              width="36"
-              height="36"
-              alt="Express"
-            />
-          </a>
-          <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg"
-              width="36"
-              height="36"
-              alt="MongoDB"
-            />
-          </a>
-          <a
-            href="https://www.postgresql.org/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/postgresql-colored.svg"
-              width="36"
-              height="36"
-              alt="PostgreSQL"
-            />
-          </a>
-          <a href="https://www.heroku.com/" target="_blank" rel="noreferrer">
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/heroku-colored.svg"
-              width="36"
-              height="36"
-              alt="Heroku"
-            />
-          </a>
-          <a
-            href="https://www.djangoproject.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/django-colored.svg"
-              width="36"
-              height="36"
-              alt="Django"
-            />
-          </a>
-          <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
-            <img
-              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/figma-colored.svg"
-              width="36"
-              height="36"
-              alt="Figma"
-            />
-          </a>
-        </div>
+        <h2> About Me </h2>
+        <hr /> 
+        <About />
       </div>
 
       <div id="projects">
---
+++ src/about/About.js
@@ -0,0 +1,159 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+  return (
+    <div>
+      <div className="about-container">
+      <img
+          src="/imgs/about-selfie.JPG"
+          alt="placeholder"
+          className="about-pic"
+        />
+        <p>
+          I have 3 years experience in managing sales and international clients
+          in the UK’s New Homes industry and a bachelor degree in Languages. My
+          interest in coding has been lingering since my early teenage years
+          where I used to run a blog and play around with basic HTML/CSS skills.
+          I believe that languages and programming languages have a lot in
+          common. The problem-solving, analytical world of programming, with its
+          endless learning possibilities, perfectly chimes with my attitude and
+          my strong desire to develop and widen my knowledge.
+        </p>
+      </div>
+
+      <h3>My Skills</h3>
+      <hr />
+      <div>
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg"
+            width="36"
+            height="36"
+            alt="JavaScript"
+          />
+        </a>
+        <a href="https://www.python.org/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/python-colored.svg"
+            width="36"
+            height="36"
+            alt="Python"
+          />
+        </a>
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg"
+            width="36"
+            height="36"
+            alt="HTML5"
+          />
+        </a>
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg"
+            width="36"
+            height="36"
+            alt="React"
+          />
+        </a>
+        <a href="https://jquery.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/jquery-colored.svg"
+            width="36"
+            height="36"
+            alt="JQuery"
+          />
+        </a>
+        <a
+          href="https://www.w3.org/TR/CSS/#css"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg"
+            width="36"
+            height="36"
+            alt="CSS3"
+          />
+        </a>
+        <a href="https://getbootstrap.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg"
+            width="36"
+            height="36"
+            alt="Bootstrap"
+          />
+        </a>
+        <a href="https://nodejs.org/en/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg"
+            width="36"
+            height="36"
+            alt="NodeJS"
+          />
+        </a>
+        <a href="https://expressjs.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-colored.svg"
+            width="36"
+            height="36"
+            alt="Express"
+          />
+        </a>
+        <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg"
+            width="36"
+            height="36"
+            alt="MongoDB"
+          />
+        </a>
+        <a href="https://www.postgresql.org/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/postgresql-colored.svg"
+            width="36"
+            height="36"
+            alt="PostgreSQL"
+          />
+        </a>
+        <a href="https://www.heroku.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/heroku-colored.svg"
+            width="36"
+            height="36"
+            alt="Heroku"
+          />
+        </a>
+        <a
+          href="https://www.djangoproject.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/django-colored.svg"
+            width="36"
+            height="36"
+            alt="Django"
+          />
+        </a>
+        <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/figma-colored.svg"
+            width="36"
+            height="36"
+            alt="Figma"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
